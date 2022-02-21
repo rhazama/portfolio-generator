f224aa2f9632345a3ff47f133901aa9c88385855
Ryan Hazama <ryanhazama@Ryans-Laptop.local>
2022-02-21 15:26:51 -0800

    changed app.js and added username and githubName

--- app.js
+++ app.js
@@ -1,11 +1,3 @@
-const printProfileData = profileDataArr => {
-    // This...
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-      console.log(profileDataArr[i]);
-    }
-  
-    console.log('================');
-  
-    // Is the same as this...
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-  };
+const generatePage = (userName, githubName) => `Name: ${userName}, Github: ${githubName}`
+
+console.log(generatePage('Jane', 'janehub'));
